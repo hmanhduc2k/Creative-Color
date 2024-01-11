cb84ad7982bbc5c1f3c914a7d1f2a0a7d604dbb4
hmanhduc2k <hmanhduc2k@gmail.com>
2024-01-11 13:39:39 +0800

    first commit

---
+++ CColour_app_2/CColour_app_2/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CColour_app_2
+{
+    [ContentProperty(nameof(Source))]
+
+    public class ImageResourceExtension : IMarkupExtension
+    {
+        public string Source { get; set; }
+
+        public object ProvideValue(IServiceProvider serviceProvider)
+        {
+            if (Source == null)
+                return null;
+
+            return ImageSource.FromResource(Source, typeof(ImageResourceExtension).GetTypeInfo().Assembly);
+        }
+
+        
+    }
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        void show(object sender, EventArgs e)
+        {
+            DisplayAlert("Message", "true", "OK");
+        }
+
+    }
+}
